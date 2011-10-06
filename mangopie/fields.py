a64eb127bc93dc6b8cec552ff5b74c6895dc3faa
Jan Schrewe <jan@schafproductions.com>
2011-10-06 17:14:10 +0200

    PEP 8

--- mangopie/fields.py
+++ mangopie/fields.py
@@ -3,32 +3,32 @@ from tastypie.resources import Resource
 from tastypie.fields    import ApiField
 
 class ListFieldValue(object):
-  def __init__(self, value):
-    self.value = value
+    def __init__(self, value):
+        self.value = value
 
 class ListField(ApiField):
-  def __init__(self, inner_field, **kwargs):
-    super(ListField, self).__init__(**kwargs)
+    def __init__(self, inner_field, **kwargs):
+        super(ListField, self).__init__(**kwargs)
 
-    inner_field.attribute = 'value'
-    
-    self.inner_field = inner_field
-  
-  def dehydrate(self, bundle):
-    items = getattr(bundle.obj, self.attribute)
+        inner_field.attribute = 'value'
 
-    return [self.inner_field.dehydrate(Bundle(obj = ListFieldValue(item))) for item in items]
+        self.inner_field = inner_field
+
+    def dehydrate(self, bundle):
+        items = getattr(bundle.obj, self.attribute)
+
+        return [self.inner_field.dehydrate(Bundle(obj = ListFieldValue(item))) for item in items]
 
 class DictField(ApiField):
-  pass
-  
+    pass
+
 class EmbeddedResourceField(ApiField):
-  def __init__(self, resource_type, **kwargs):
-    super(EmbeddedResourceField, self).__init__(**kwargs)
+    def __init__(self, resource_type, **kwargs):
+        super(EmbeddedResourceField, self).__init__(**kwargs)
 
-    self.resource_type = resource_type
-    
-  def dehydrate(self, bundle):
-      doc = getattr(bundle.obj, self.attribute)
+        self.resource_type = resource_type
 
-      return self.resource_type().full_dehydrate(doc)
+    def dehydrate(self, bundle):
+        doc = getattr(bundle.obj, self.attribute)
+
+        return self.resource_type().full_dehydrate(doc)
