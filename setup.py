b5c68da4bf38cd5a3356b742e4aaeb5503e27224
Jan Schrewe <jan@schafproductions.com>
2011-10-08 04:50:39 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mangopie',
+    version='0.1a',
+    description="Mongoengine documents for tastypie.",
+    author='Jan Schrewe',
+    author_email='jan@schafproductions.com',
+    url='http://www.schafproductions.com',
+    packages=['mangopie',],
+)
